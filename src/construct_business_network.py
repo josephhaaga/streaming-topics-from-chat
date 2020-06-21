46adabb5743ac0c24e6b2d13cd5fc98de0698f5f
Joseph Haaga <haaga.joe@gmail.com>
2020-06-21 13:54:42 -0400

    add error message

--- src/construct_business_network.py
+++ src/construct_business_network.py
@@ -38,10 +38,28 @@ def match_text_to_event_handler(text):
     if len(usernames) < 2:
         return
     origin, dest = usernames
+    # arsing economic event: @joehaaga gave 1 street cred to @l0gic23 @l0gic23 @aquafunkalisticbootywhap @znawaz123 @aquafunkalisticbootywhap each
+    # raceback (most recent call last):
+    #  File "src/construct_business_network.py", line 59, in <module>
+    #    if __name__ == '__main__':
+    #  File "src/construct_business_network.py", line 56, in main
+    #    print(f"Following economic news on {channel}")
+    #  File "src/construct_business_network.py", line 17, in follow_the_stream
+    #    parse_economic_event
+    #  File "/Users/josephhaaga/Documents/code/streaming-topics-from-chat/src/utils.py", line 31, in listen_for_messages
+    #    handler(message)
+    #  File "src/construct_business_network.py", line 29, in parse_economic_event
+    #    match_text_to_event_handler(a['text'].lower())
+    #  File "src/construct_business_network.py", line 40, in match_text_to_event_handler
+    #    origin, dest = usernames
+    # alueError: too many values to unpack (expected 2)
+
+
     if 'stole from' in text:
         # "@bopojoe_ stole from @joehaaga. Chance of Getting Caught: 51.66666666666666%"
         insert_event_into_graph(origin, "ROBBED_SUCCESSFULLY", dest)
     elif 'was caught stealing' in text:
+        # Beginbotbot doesn't list the dest, so we don't pass the len >= 2 check
         insert_event_into_graph(origin, "ROBBED_FAILED", dest)
     elif 'shared' in text:
         # PRO-SOCIAL BEHAVIOR
