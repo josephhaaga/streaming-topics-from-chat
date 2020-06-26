91c25d21d515e347c445bf873dbed99255bcdce3
Joseph Haaga <haaga.joe@gmail.com>
2020-06-25 23:07:45 -0400

    simple robinhood script

---
+++ src/robinhood.py
@@ -0,0 +1,25 @@
+import requests
+import random
+HOST = "https://mygeoangelfirespace.city"
+a = requests.get(f"{HOST}/db/users.json").json()
+
+sorted(a['users'].values(), key=lambda k: k['street_cred'])
+
+population = len(a['users'].values())
+
+the_one_percent = sorted(a['users'].values(), key = lambda k: k['cool_points'])[-1*(population//100):]
+
+target = random.choice([person['name'] for person in the_one_percent])
+
+# Get a sound effect from the target
+b = requests.get(f"{HOST}/{target}.html").content.split(b'ul')[1].split(b'\n')
+commands = [x.replace(b'        <a href="https://mygeoangelfirespace.city/commands/', b'').replace(b'.html">', b'') for x in b if b'commands' in x]
+
+bounty = random.choice(commands).decode('utf8')
+
+# find a poor active player
+beneficiary = 'joehaaga'
+
+# print steal and give commands
+print(f"!steal @{target} {bounty}")
+
